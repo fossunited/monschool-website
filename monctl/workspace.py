33667fa7f680d2e2292cc781d5c3a926e0dcc751
Anand Chitipothu <anandology@gmail.com>
2021-10-20 16:02:10 +0530

    workspace - added functions to read chapter and lesson

--- monctl/workspace.py
+++ monctl/workspace.py
@@ -5,7 +5,12 @@ monctl.workspace
 Module to read the courses, chapters, lessons from the disk.
 """
 from pathlib import Path
+import frontmatter
 import yaml
+
+FIELDS = {
+    "LMS Course": "title instructor is_published short_introduction description chapters"
+}
 
 class Workspace:
     def __init__(self, root="courses"):
@@ -14,10 +19,8 @@ class Workspace:
 
     def read_yaml_file(self, path: Path):
         if path not in self.cache:
-            d = yaml.safe_load(path.open())
-            self.cache[path] = d
+            self.cache[path] = yaml.safe_load(path.open())
         data = self.cache[path]
-        print(data)
         return dict(data)
 
     def read_course(self, name):
@@ -27,3 +30,28 @@ class Workspace:
         course['chapters'] = [c['name'] + "-" + name_suffix for c in course['chapters']]
         return course
 
+    def read_chapter(self, course_name, name):
+        path = self.root.joinpath(course_name, "course.yml")
+        course = self.read_yaml_file(path)
+        name_suffix = course['suffix']
+        for c in course['chapters']:
+            if c['name'] == name:
+                doc = dict(c)
+                doc['name'] = doc['name'] + "-" + name_suffix
+                doc['lessons'] = [Path(p).stem + "-" + name_suffix for p in doc['lessons']]
+                return doc
+
+    def read_lesson(self, path: Path):
+        course_yml = path.parent.parent.joinpath("course.yml")
+        # print("course_yml")
+        course = self.read_yaml_file(course_yml)
+        name_suffix = course['suffix']
+
+        text = path.read_text()
+        data = frontmatter.loads(text)
+
+        doc = dict(data)
+        doc['name'] = path.stem + "-" + name_suffix
+        doc['body'] = data.content.strip()
+        doc['chapter'] = path.parent.stem + "-" + name_suffix
+        return doc
--- tests/test_workspace.py
+++ tests/test_workspace.py
@@ -12,8 +12,20 @@ is_published: 0
 chapters:
   - name: introduction
     title: Introduction
+    lessons:
+      - introduction/getting-started.md
+      - introduction/hello-world.md
   - name: datatypes
     title: Datatypes
+"""
+
+LESSON_TEXT = """\
+---
+title: Getting Started
+include_in_preview: 0
+---
+
+Getting Started with Python
 """
 
 class TestWorkspace:
@@ -32,4 +44,34 @@ class TestWorkspace:
             "instructor": "foobar",
             "is_published": 0,
             "chapters": ["introduction-pp", "datatypes-pp"]
+        }
+
+    def test_read_chapter(self, tmp_path):
+        path = tmp_path / "python-primer" / "course.yml"
+        path.parent.mkdir()
+        path.write_text(COURSE_YML)
+
+        w = workspace.Workspace(str(tmp_path))
+        assert w.read_chapter("python-primer", "introduction") == {
+            "name": "introduction-pp",
+            "title": "Introduction",
+            "lessons": ["getting-started-pp", "hello-world-pp"]
+        }
+
+    def test_read_lesson(self, tmp_path):
+        path = tmp_path / "python-primer" / "course.yml"
+        path.parent.mkdir()
+        path.write_text(COURSE_YML)
+
+        path2 = path.parent.joinpath("introduction", "getting-started.md")
+        path2.parent.mkdir()
+        path2.write_text(LESSON_TEXT)
+
+        w = workspace.Workspace(str(tmp_path))
+        assert w.read_lesson(path2) == {
+            "name": "getting-started-pp",
+            "chapter": "introduction-pp",
+            "title": "Getting Started",
+            "include_in_preview": 0,
+            "body": "Getting Started with Python"
         }
